7fe934ddbc6f08b26d5a477573eeec3d73f37978
Dmitry <43281522+KartonDCP@users.noreply.github.com>
2020-05-23 19:48:38 +0300

    add errors

--- eclipse-workspace/MobileServer/src/com/KartonDCP/MobileSever/ProtocolSDK/ProtocolParser.java
+++ eclipse-workspace/MobileServer/src/com/KartonDCP/MobileSever/ProtocolSDK/ProtocolParser.java
@@ -1,15 +1,42 @@
 package com.KartonDCP.MobileSever.ProtocolSDK;
 
+import java.util.Map;
+
 public class ProtocolParser {
     private final String requestStr;
+    private final String token;
+    private String[] split;
 
-    public ProtocolParser(String requestStr){
-        this.requestStr = requestStr;
+    public ProtocolParser(String requestStr, String token){
+        this.requestStr = requestStr.trim().replace(" ", "")
+                .replace("\n", "")
+                .replace("\t", "");
+        this.token = token;
+
+        if(!correctFormat()){
+            throw new
+        }
+
     }
+
+    public boolean correctFormat(){
+        if(requestStr.contains("?")){
+            split = requestStr.split("/?");
+            return split.length == 3 && split[0].equals(token);
+        }
+        else {
+            return false;
+        }
+
+    }
+
+
     public ProtocolMethod getMethodName(){
         if(requestStr.contains("?")){
-            
+
         }
     }
-    public
+    public Map<String, String> getArgs(){
+
+    }
 }
---
+++ eclipse-workspace/MobileServer/src/com/KartonDCP/MobileSever/Utils/Exceptions/TokenException.java
@@ -0,0 +1,9 @@
+package com.KartonDCP.MobileSever.Utils.Exceptions;
+
+public class TokenException extends InvalidRequestException {
+
+    public TokenException(String message) {
+        super(message);
+    }
+}
+
