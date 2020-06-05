5b81d431f9d93753945328ee5fc8970901cde637
Dmitry <43281522+KartonDCP@users.noreply.github.com>
2020-06-05 20:34:32 +0300

    on new methods

---
+++ eclipse-workspace/MobileServer/src/com/KartonDCP/Server/MobileSever/OperationWorker/GetLastEvents.java
@@ -0,0 +1,22 @@
+package com.KartonDCP.Server.MobileSever.OperationWorker;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.concurrent.ExecutionException;
+
+public class  GetLastEvents implements OperationWorker {
+    @Override
+    public boolean executeWorkSync() throws SQLException, NoSuchFieldException, IOException {
+        return false;
+    }
+
+    @Override
+    public boolean executeWorkAsync() throws SQLException, IOException, ExecutionException, InterruptedException {
+        return false;
+    }
+
+    @Override
+    public boolean cancel() {
+        return false;
+    }
+}
